07caeb154f1342ed78fc754e63559f5bc27057ab
rtpaguila <rtpaguila@up.edu.ph>
2023-08-11 15:59:22 +0800

    fix: functionality of functions
    Ensure accurate functionality for functions as they previously generated cut words. Had to separate handling of @@ and ## separately as they have different behaviors

--- pos_tagging.py
+++ pos_tagging.py
@@ -29,9 +29,9 @@ def overall_count(row_records):
     print("hello")
 
 def standardize_labels(label):
-    if label in ['JJ', 'ADJ','X']:
+    if label in ['JJ', 'ADJ','X', 'ADV']:
         return 'Adjective'
-    if label in ['VB', 'VBD', 'VERB']:
+    if label in ['VB', 'VBD', 'VERB', 'VBN']:
         return 'Verb'
     if label in ['PRON', 'NOUN','PROPN', 'NN', 'NNP']:
         return 'Noun'
@@ -58,53 +58,91 @@ def initialize_pos_models():
 
     return models
 
-
-def identify_pos(pipeline_output):
+def identify_pos_bertweet(pipeline_output):
     # Initialize a dictionary to store the last word for each type
-    last_word = {'Noun': None, 'Verb': None, 'Adjective': None, 'Others': None}
+    last_word = {'Noun': '', 'Verb': '', 'Adjective': '', 'Others': '', 'General': ''}
     # Initialize a dictionary to store the counts for each type
     counts = {'Noun': {}, 'Verb': {}, 'Adjective': {}, 'Others': {}}
     at_flag = 0
     # Start processing the pipeline output
     for result in pipeline_output:
         # Words attached with `##` are subwords
-        if '##' in result['word']:
-            # Attach subword to last word based on its entity (part of speech) type
-            entity_type = standardize_labels(result['entity'])
-            if last_word[entity_type] is not None:
-                temp = last_word[entity_type]
-                last_word[entity_type] += result['word'].replace('##', '')
-                counts[entity_type][last_word[entity_type]] = counts[entity_type].get(last_word[entity_type], 0) + 1
-                last_word[entity_type] = None
-                counts[entity_type].pop(temp)
-            continue
+        entity_type = standardize_labels(result['entity'])
+
         if '@@' in result['word']:
-            # print(result)
-            # Attach subword to last word based on its entity (part of speech) type
-            entity_type = standardize_labels(result['entity'])
-            last_word[entity_type] = result['word'].replace('@@', '')
-            at_flag = 1
+            
+            if last_word['General'] == '':
+                last_word['General'] = result['word'].replace('@@', '')
+                at_flag = 1
+            else:
+                last_word['General'] = last_word['General'] + result['word'].replace('@@', '')
             continue
-        if at_flag:
+        if at_flag == 1:
             # @@ is used by TweebankNLP_Bert. It behaves differently from ## as ## is placed at the beginning of the second half
             # of the word while @@ is put at the end of the first half of the word hence they need to be processed
             # differently as well
-            result['word'] = last_word[entity_type] + result['word']
+            result['word'] = last_word['General'] + result['word']
+            last_word['General'] = result['word'] 
             at_flag = 0
-        # For full words, process them based on their entity type
+            counts[entity_type][last_word['General']] = counts[entity_type].get(last_word['General'], 0) + 1
+            last_word['General'] = ''
+            continue
+        
+        last_word['General'] = result['word']
+        counts[entity_type][last_word['General']] = counts[entity_type].get(last_word['General'], 0) + 1
+        last_word['General'] = ''
+
+    return counts['Noun'], counts['Verb'], counts['Adjective'], counts['Others']
+
+def identify_pos(pipeline_output):
+    # Initialize a dictionary to store the last word for each type
+    last_word = {'Noun': '', 'Verb': '', 'Adjective': '', 'Others': '', 'General': ''}
+    # Initialize a dictionary to store the counts for each type
+    counts = {'Noun': {}, 'Verb': {}, 'Adjective': {}, 'Others': {}}
+    at_flag = 0
+    hash_flag = 0
+    # Start processing the pipeline output
+    for result in pipeline_output:
+        print(result)
+        # Words attached with `##` are subwords
         entity_type = standardize_labels(result['entity'])
-        last_word[entity_type] = result['word']
-        counts[entity_type][last_word[entity_type]] = counts[entity_type].get(last_word[entity_type], 0) + 1
+        if '##' in result['word']:
+            # Attach subword to last word based on its entity (part of speech) type
+            try:
+                counts[entity_type].pop(last_word['General'])
+            except:
+                print()
+            hash_flag = 1
+            last_word['General'] = last_word['General'] + result['word'].replace('##', '')
+            
+            # Case the word is the last in the sentence
+            if result == pipeline_output[-1]:
+                counts[entity_type][last_word['General']] = counts[entity_type].get(last_word['General'], 0) + 1
+            continue
+        if hash_flag == 1:
+            counts[entity_type][last_word['General']] = counts[entity_type].get(last_word['General'], 0) + 1
+            hash_flag = 0
+            last_word['General'] = result['word']
+            if result == pipeline_output[-1]:
+                counts[entity_type][last_word['General']] = counts[entity_type].get(last_word['General'], 0) + 1
+            continue
+        counts[entity_type][result['word']] = counts[entity_type].get(last_word['General'], 0) + 1
+        last_word['General'] = result['word']
 
     return counts['Noun'], counts['Verb'], counts['Adjective'], counts['Others']
 
 def process_row(models, row):
+    # I also need to take the sentiment into account when summarizing
     # these are turned into # when tokenized. Simply remove them beforehand
     review = row.Review.replace(',','').replace('.','')
     # tokenizer = TweetTokenizer()
     try:
         for model, noun_field, verb_field, adjective_field, others_field in models:
-            nouns, verbs, adjectives, others = identify_pos(model(review))
+            print(others_field)
+            if noun_field == 'Nouns_TweebankNLP_Bert':
+                nouns, verbs, adjectives, others = identify_pos_bertweet(model(review))
+            else:
+                nouns, verbs, adjectives, others = identify_pos(model(review))
             row = row._replace(**{noun_field: nouns, verb_field: verbs, adjective_field: adjectives, others_field: others})
     except Exception as e:
         # print()
